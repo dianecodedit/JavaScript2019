fadfd00bcfa5a62b21c83a54ba39bc46b05cc300
dianecodedit <diane@codedit.tech>
2019-07-14 12:24:39 +1200

    Revert "Odin Project"
    This reverts commit 78e22cd75c74211c066e1c8b61aa6b1e56eb47ce.

--- Odin Project/javascript-exercises-master/javascript-exercises-master/reverseString/reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(str) {
-    strArr = []
-    strArr = str.split("")
-    strArr = strArr.reverse()
-    str = strArr.join("")
-    return str
-}
-
-module.exports = reverseString
